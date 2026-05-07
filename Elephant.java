c25b30ce821ff840ad23745e5f73d1b7b5908dc2
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-05-07 10:58:16 -0400

    animated elephant

--- Elephant.java
+++ Elephant.java
@@ -9,11 +9,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("comical elephant fart.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant-idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[10];
     
     public Elephant()
     {
-        setImage(idle);
+        for (int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant-idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
     }
     
     public void act()
@@ -29,6 +40,8 @@ public class Elephant extends Actor
         }
         
         eat();
+        
+        animateElephant();
     }
     
     public void eat()
